213e72df5f5680511a9be851ca366f550af0871a
Johannes Lier <johannes.o.lier@gmail.com>
2015-03-02 14:31:24 -0800

    First commit

---
+++ android/src/com/joejohn/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.joejohn.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.joejohn.game.DualRacer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DualRacer(), config);
+	}
+}
---
+++ desktop/src/com/joejohn/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.joejohn.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.joejohn.game.DualRacer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DualRacer(), config);
+	}
+}
